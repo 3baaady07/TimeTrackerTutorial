cdaa183a73986dc3ca08c4cc11e853521350c2e5
Abdullah Alharbi <abdullah@excelguru.ca>
2021-03-06 14:08:53 -0800

    Episode 10: Implemented AccountService for Android

---
+++ TimeTrackerTutorial/TimeTrackerTutorial.Android/Services/AccountService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Firebase.Auth;
+using TimeTrackerTutorial.Droid.Services;
+using TimeTrackerTutorial.Services.Account;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(AccountService))]
+namespace TimeTrackerTutorial.Droid.Services
+{
+    public class AccountService : IAccountService
+    {
+        public Task<double> GetCurrentPayRateAsync()
+        {
+            return Task.FromResult(10d);
+        }
+
+        public Task<bool> LoginAsync(string username, string password)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            FirebaseAuth.Instance.SignInWithEmailAndPasswordAsync(username,password)
+                .ContinueWith((task) => OnAuthCompleted(task, tcs));
+
+            return tcs.Task;
+        }
+
+        private void OnAuthCompleted(Task task, TaskCompletionSource<bool> tcs)
+        {
+            if (task.IsCanceled || task.IsFaulted)
+            {
+                //Something went wrong
+                tcs.SetResult(false);
+                return;
+            }
+            tcs.SetResult(true);
+        }
+    }
+}
